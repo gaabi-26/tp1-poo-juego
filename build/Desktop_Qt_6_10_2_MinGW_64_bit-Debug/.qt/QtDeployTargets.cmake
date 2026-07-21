bb128bb605a340ab1c0494bcea97dcb3e0e42dd7
Fernando Tentor <fertentor@gmail.com>
2026-07-21 15:52:10 -0300

    Ignorar carpeta build

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TPpoo2da_FILE C:/Users/ferte/OneDrive/Desktop/Facu 2026/POO/Tp Poo/TPpoo2da/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/TPpoo2da.exe)
-set(__QT_DEPLOY_TARGET_TPpoo2da_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TPpoo2da_RUNTIME_DLLS D:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/CMakeFiles/TPpoo2da.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TPpoo2da.
-# This may be replaced when dependencies are built.
